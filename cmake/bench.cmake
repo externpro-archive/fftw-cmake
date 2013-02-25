61c6f98932ff6a78dc518b61f7118edb21b28f1f
Scott M Anderson <smanders@sdl.usu.edu>
2013-02-25 08:16:34 -0700

    include bench headers in cmake

--- cmake/bench.cmake
+++ cmake/bench.cmake
@@ -14,14 +14,18 @@ list(APPEND ${exe_name}_srcs ${cmake_srcs})
 ########################
 # bench
 file(GLOB bench_srcs ${fftwroot}/tests/*.c)
+file(GLOB bench_hdrs ${fftwroot}/tests/*.h)
 source_group("Source Files\\bench" FILES ${bench_srcs})
-list(APPEND ${exe_name}_srcs ${bench_srcs})
+source_group("Header Files\\bench" FILES ${bench_hdrs})
+list(APPEND ${exe_name}_srcs ${bench_srcs} ${bench_hdrs})
 ########################
 # libbench2
 file(GLOB libbench2_srcs ${fftwroot}/libbench2/*.c)
 list(REMOVE_ITEM libbench2_srcs ${fftwroot}/libbench2/useropt.c)
+file(GLOB libbench2_hdrs ${fftwroot}/libbench2/*.h)
 source_group("Source Files\\libbench2" FILES ${libbench2_srcs})
-list(APPEND ${exe_name}_srcs ${libbench2_srcs})
+source_group("Header Files\\libbench2" FILES ${libbench2_hdrs})
+list(APPEND ${exe_name}_srcs ${libbench2_srcs} ${libbench2_hdrs})
 #######################################
 # executable
 add_executable(${exe_name} ${${exe_name}_srcs})
